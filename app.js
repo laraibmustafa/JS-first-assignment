bc4a7e121b8def7eafe9e404fd1abd3b0f11843c
laraibmustafa <143021177+laraibmustafa@users.noreply.github.com>
2023-09-29 20:33:54 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+alert("hello world!");
+
+//let a =10;
+
+console.log(" welcome to js ");
+prompt("how are you");
+
+//console.log("");outputs a message to the web console.
+
+/*prompt("Hi,how are you?");
+prompt(" What is your name?");*/
